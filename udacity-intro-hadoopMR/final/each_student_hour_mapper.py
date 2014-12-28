7b5867e8582c19b18cfa1d261c65f2bebc9be85d
1ambda <1amb4a@gmail.com>
2014-12-28 12:27:24 +0900

    each student hour

---
+++ udacity-intro-hadoopMR/final/each_student_hour_mapper.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import sys
+import csv
+import re
+
+# test command:
+# > cat small_forum.csv | sed 1d | python mapper.py | python header.py
+def mapper():
+    reader = csv.reader(sys.stdin, delimiter='\t')
+    for row in reader:
+        if len(row) != 19:
+            continue
+
+        author_id = row[3]  # e.g 100005361 
+        added_at  = row[8]  # e.g 2012-02-23 00:28:02.321344+00
+        hour = added_at.split()[1].split(':')[0]
+
+        print "{0}\t{1}".format(author_id, hour)
+
+
+def main():
+    try:
+        mapper()
+    except:
+        pass
+
+main()
---
+++ udacity-intro-hadoopMR/final/each_student_hour_reducer.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import sys
+
+# test command:
+# > cat small_forum.csv | sed 1d | python mapper.py | python header.py
+def padding(hour):
+    h = str(hour)
+    if len(h) == 1:
+        return "0" + h
+    else:
+        return h
+
+
+def reducer():
+    prev_id = None
+    hours = [0] * 24
+
+    for row in sys.stdin:
+        data = row.strip().split("\t")
+
+        if len(data) != 2:
+            continue
+
+        author_id, hour = data
+        hour = int(hour)
+
+        if prev_id and prev_id != author_id:
+            mostActiveHour = hours.index(max(hours))
+            print "{0}\t{1}".format(author_id, padding(mostActiveHour))
+            hours = [0] * 24
+
+        prev_id = author_id
+        hours[hour] += 1
+
+    if prev_id:
+        mostActiveHour = hours.index(max(hours))
+        print "{0}\t{1}".format(author_id, padding(mostActiveHour))
+
+
+def main():
+    reducer()
+
+if __name__ == '__main__':
+    main()
